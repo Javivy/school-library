5f2d30277a4587f3c6b58733af2feb9c029599cb
Javier Vargas <javierralejandroortga@gmail.com>
2023-03-06 14:42:28 -0400

    Revert "Design pattern"

--- person.rb
+++ person.rb
@@ -1,21 +1,12 @@
-require './nameable'
-require './capitalize_decorator'
-require './trimmer_decorator'
-
-class Person < Nameable
+class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def correct_name
-    @name
   end
 
   def can_use_services?
