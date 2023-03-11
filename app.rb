175dad05e7b65f58b9bbdbdae9c7ee8dadf987a8
Javivy <84439052+Javivy@users.noreply.github.com>
2023-03-10 20:02:43 -0400

    Fix linters errors

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
-require "./teacher.rb"
-require "./student.rb"
-require "./book.rb"
-require "./rental.rb"
+require './teacher'
+require './student'
+require './book'
+require './rental'
 
 ACTIONS = {
   1 => :list_all_books,
@@ -52,45 +52,43 @@ class App
   end
 
   def list_all_books
-    books.each do |book, index|
+    books.each do |book, _index|
       puts("Title: #{book.title}, Author: #{book.author}")
     end
   end
 
   def list_all_people
     people.each do |person|
-      puts("[#{person.is_a?(Student) ? "Student" : "Teacher"}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}")
+      puts("[#{person.is_a?(Student) ? 'Student' : 'Teacher'}]
+      Name: #{person.name},
+      ID: #{person.id},
+      Age: #{person.age}")
     end
   end
 
   def create_person
-    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     number = gets.chomp.to_i
 
-    if number == 1
-      print("Age: ")
+    case number
+    when 1
+      print('Age: ')
       age = gets.chomp
-      
-      print("Name: ")
+      print('Name: ')
       name = gets.chomp
-      
-      print("Has parent permission? [Y/N]:")
+      print('Has parent permission? [Y/N]:')
       parent_permission_console = gets.chomp
-      
-      @people.push(Student.new(age, name, parent_permission_console == "Y" ? parent_permission = true : parent_permission = false))
-      puts("Person created successfully")
-    elsif number == 2
-      print("Age: ")
+      @people.push(Student.new(age, name, parent_permission_console == 'Y'))
+      puts('Person created successfully')
+    when 2
+      print('Age: ')
       age = gets.chomp
-
-      print("Specialization: ")
+      print('Specialization: ')
       specialization = gets.chomp
-
-      print("Name: ")
+      print('Name: ')
       name = gets.chomp
-
       @people.push(Teacher.new(age, specialization, name))
-      puts("Person created successfully")
+      puts('Person created successfully')
     else
       puts("The number #{number} isn't a valid choice, please select number (1) to create a student or (2) to create
       a teacher")
@@ -98,42 +96,47 @@ class App
   end
 
   def create_book
-    print("Title: ")
+    print('Title: ')
     title = gets.chomp
 
-    print("Author: ")
+    print('Author: ')
     author = gets.chomp
 
     @books.push(Book.new(title, author))
-    puts("The book has been created successfully")
+    puts('The book has been created successfully')
   end
 
   def create_rental
-    puts("Select a book from the following list by number")
+    puts('Select a book from the following list by number')
     books.each_with_index do |book, index|
       puts("#{index}) Title: #{book.title}, Author: #{book.author}")
     end
     book_number = gets.chomp.to_i
     book_selected = books[book_number]
 
-    puts("Select a person from the following list by number (not ID)")
+    puts('Select a person from the following list by number (not ID)')
     people.each_with_index do |person, index|
-      puts("#{index}) [#{person.is_a?(Student) ? "Student" : "Teacher"}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}")
+      puts(
+        "#{index}) [#{person.is_a?(Student) ? 'Student' : 'Teacher'}]
+      Name: #{person.name},
+      ID: #{person.id},
+      Age: #{person.age}"
+      )
     end
-    person_number = gets.chomp.to_i 
+    person_number = gets.chomp.to_i
     person_selected = people[person_number]
 
-    print("Date: ")
+    print('Date: ')
     date = gets.chomp
 
     @rentals.push(Rental.new(date, person_selected, book_selected))
   end
 
   def list_rentals_by_person_id
-    print("ID of person: ")
+    print('ID of person: ')
     id = gets.chomp.to_i
 
-    puts "Rentals: "
+    puts 'Rentals: '
     @rentals.each do |rental|
       puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
     end
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unkown', parent_permission = true)
-    super(age, name, parent_permission:parent_permission)
+  def initialize(age, specialization, name = 'Unkown', parent_permission: true)
+    super(age, name, parent_permission:)
 
     @specialization = specialization
   end
